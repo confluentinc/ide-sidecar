b451b2278aece6d9ebd0767f276748f867b5aa6e
Rohit Sanjay <rsanjay@confluent.io>
2024-09-05 15:40:37 -0700

    Retry flaky test three times before failing

--- src/test/java/io/confluent/idesidecar/restapi/resources/ConfluentCloudQueryResourceTest.java
+++ src/test/java/io/confluent/idesidecar/restapi/resources/ConfluentCloudQueryResourceTest.java
@@ -20,6 +20,8 @@ import io.quarkus.test.junit.TestProfile;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junitpioneer.jupiter.ExpectedToFail;
+import org.junitpioneer.jupiter.RetryingTest;
 
 @QuarkusTest
 @ConnectWireMock
@@ -181,7 +183,9 @@ public class ConfluentCloudQueryResourceTest extends ConfluentQueryResourceTestB
     );
   }
 
+  // TODO: Fix flaky test. Retry three times before failing.
   @Test
+  @RetryingTest(3)
   void shouldCacheClusterInfoOnGetKafkaClusters() {
     // Create a local connection
     ccloudTestUtil.createConnection("local-1", "Local 1", ConnectionType.LOCAL);
