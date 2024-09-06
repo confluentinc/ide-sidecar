c7ae68847a206bf145142c8cacd710ea5312e536
Rohit Sanjay <rsanjay@confluent.io>
2024-09-06 10:23:59 -0700

    Cache ~/.m2/repository in multi-arch-builds (#21)

--- .semaphore/build_native_executable_epilogue.sh
+++ .semaphore/build_native_executable_epilogue.sh
@@ -1,2 +1,3 @@
 NATIVE_EXECUTABLE=$(find target -name "*-runner")
 artifact push workflow ${NATIVE_EXECUTABLE} --destination native-executables/$(basename ${NATIVE_EXECUTABLE}-${OS}-${ARCH})
+make ci-bin-sem-cache-store
