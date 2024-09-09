8661279919c6728527723fea2b54f99d5cd5ca3d
Rohit Sanjay <rsanjay@confluent.io>
2024-09-09 12:14:17 -0700

    Remove sem cache commands out of prologue and epilogue for multi-arch-builds

--- .semaphore/build_native_executable_prologue.sh
+++ .semaphore/build_native_executable_prologue.sh
@@ -9,4 +9,3 @@ curl -s "https://get.sdkman.io?rcupdate=false" | bash
 source "$HOME/.sdkman/bin/sdkman-init.sh"
 # Install GraalVM as defined in .sdkmanrc
 sdk env install
-make ci-bin-sem-cache-restore
