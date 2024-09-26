35684287e64eeb53e6db28bae83a858572f8e9eb
Stefan Sprenger <128683+flippingbits@users.noreply.github.com>
2024-09-26 08:26:07 +0200

    feat: Make settings related to SSL certificates configurable via Preferences API (#24) (#64)
    * feat: Make settings related to SSL certificates configurable via Preferences API
    
    A few users of [Confluent for VS Code](https://github.com/confluentinc/vscode) (https://github.com/confluentinc/vscode/issues/280 https://github.com/confluentinc/vscode/issues/268) reported that they ran into errors when working with Confluent Cloud and sitting behind a firewall that performs SSL inspection.
    
    This change introduces the Preferences API that can be used to store user preferences in the sidecar.
    
    In the beginning, it supports two preferences related to SSL handshaking:
    * The preference `tls_pem_paths` can be used to point the sidecar to one or multiple custom certificates in the PEM format (by default, no custom certificate is configured).
    * The preference `trust_all_certificates` can be used to enable/disable the verification of the server's certificate (by default, verification is enabled).
    
    Whenever the preferences change, a [CDI event](https://quarkus.io/guides/cdi#events-and-observers) gets produced so that beans relying on the preferences
    get notified. This change updates the `WebClientFactory` to `@Observe` the events related to the Preferences API so that
    the configuration of the web client is updated whenever user change their preferences. In the future, other beans consuming
    the preferences may `@Observe` the events, too.
    
    Fixes #63
    
    * chore: Remove empty lines
    
    * chore: Naming
    
    * test: Verify that updating the preferences changes the web client config
    
    * chore: Use var consistently

--- src/main/java/io/confluent/idesidecar/restapi/util/WebClientFactory.java
+++ src/main/java/io/confluent/idesidecar/restapi/util/WebClientFactory.java
@@ -1,9 +1,15 @@
 package io.confluent.idesidecar.restapi.util;
 
+import io.confluent.idesidecar.restapi.models.Preferences;
+import io.confluent.idesidecar.restapi.models.Preferences.PreferencesSpec;
 import io.quarkus.arc.Arc;
+import io.quarkus.logging.Log;
 import io.vertx.core.Vertx;
+import io.vertx.core.net.PemTrustOptions;
 import io.vertx.ext.web.client.WebClient;
+import io.vertx.ext.web.client.WebClientOptions;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.event.Observes;
 import jakarta.inject.Inject;
 
 /**
@@ -33,16 +39,45 @@ public class WebClientFactory {
    */
   private WebClient webClient;
 
-  public WebClient getWebClient() {
+  public synchronized WebClient getWebClient() {
     if (webClient == null) {
-      // Will be null when not injected via CDI.
-      if (vertx == null) {
-        // Look up the Vertx instance from the CDI container.
-        vertx = Arc.container().select(Vertx.class).get();
-      }
-      webClient = WebClient.create(vertx);
+      webClient = WebClient.create(getVertx());
     }
 
     return webClient;
+  }
+
+  /**
+   * Observer method that is called whenever the {@link Preferences} are updated. Updates the
+   * configuration of the web client according to the passed {@link Preferences} instance.
+   *
+   * @param preferences The new {@link Preferences}
+   */
+  public synchronized void updateWebClientOptions(@Observes PreferencesSpec preferences) {
+    var clientOptions = new WebClientOptions();
+
+    clientOptions.setTrustAll(Boolean.TRUE.equals(preferences.trustAllCertificates()));
+
+    if (preferences.tlsPemPaths() != null) {
+      var pemTrustOptions = new PemTrustOptions();
+      for (var pemPath : preferences.tlsPemPaths()) {
+        pemTrustOptions.addCertPath(pemPath);
+      }
+      clientOptions.setPemTrustOptions(pemTrustOptions);
+    }
+
+    webClient = WebClient.create(getVertx(), clientOptions);
+
+    Log.debugf("Updated the Vert.x web client config to: %s", clientOptions);
+  }
+
+  private Vertx getVertx() {
+    // Will be null when not injected via CDI.
+    if (vertx == null) {
+      // Look up the Vertx instance from the CDI container.
+      vertx = Arc.container().select(Vertx.class).get();
+    }
+
+    return vertx;
   }
 }
---
+++ src/test/java/io/confluent/idesidecar/restapi/resources/PreferencesResourceTest.java
@@ -0,0 +1,248 @@
+package io.confluent.idesidecar.restapi.resources;
+
+import static io.confluent.idesidecar.restapi.util.ResourceIOUtil.asJson;
+import static io.confluent.idesidecar.restapi.util.ResourceIOUtil.loadResource;
+import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import io.confluent.idesidecar.restapi.testutil.NoAccessFilterProfile;
+import io.confluent.idesidecar.restapi.util.WebClientFactory;
+import io.quarkus.test.common.http.TestHTTPEndpoint;
+import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.junit.TestProfile;
+import io.vertx.core.net.PemTrustOptions;
+import io.vertx.ext.web.client.WebClient;
+import io.vertx.ext.web.client.WebClientOptions;
+import jakarta.inject.Inject;
+import java.util.Collections;
+import java.util.List;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+@QuarkusTest
+@TestProfile(NoAccessFilterProfile.class)
+@TestHTTPEndpoint(PreferencesResource.class)
+@TestMethodOrder(OrderAnnotation.class)
+public class PreferencesResourceTest {
+  @Inject
+  WebClientFactory webClientFactory;
+
+  @Test
+  @Order(1)
+  void getPreferencesShouldReturnAnEmptySpecByDefault() {
+    var expectedResponse = asJson(
+        loadResource("preferences/get-preferences-initial-response.json")
+    );
+
+    var responseBody = given()
+        .when()
+        .get()
+        .then()
+        .statusCode(200)
+        .extract()
+        .body()
+        .asString();
+    var responseJson = asJson(responseBody);
+
+    assertEquals(expectedResponse, responseJson);
+  }
+
+  @Test
+  @Order(2)
+  void updatePreferencesShouldAllowToUpdateSinglePreferences() {
+    var expectedResponse = asJson(
+        loadResource("preferences/update-single-preference-response.json")
+    );
+
+    var responseBody = given()
+        .when()
+        .body(
+            """
+            {
+              "api_version": "gateway/v1",
+              "kind": "Preferences",
+              "spec": {
+                "trust_all_certificates": true
+              }
+            }
+            """
+        )
+        .header("Content-Type", "application/json")
+        .put()
+        .then()
+        .statusCode(200)
+        .extract()
+        .body()
+        .asString();
+    var responseJson = asJson(responseBody);
+
+    assertEquals(expectedResponse, responseJson);
+  }
+
+  @Test
+  @Order(3)
+  void updatePreferencesShouldUpdateTheConfigOfTheWebClient() {
+    var expectedResponse = loadResource("preferences/update-multiple-preferences-response.json");
+
+    // Verify that the web client does not trust any custom cert by default
+    var trustedCerts = getTrustedCertsOfWebClient(webClientFactory.getWebClient());
+    assertTrue(trustedCerts.isEmpty());
+
+    var certPath = getCertPath("certs/artificial-rootca.pem");
+    var responseBody = given()
+        .when()
+        .body(
+            """
+            {
+              "api_version": "gateway/v1",
+              "kind": "Preferences",
+              "spec": {
+                "trust_all_certificates": true,
+                "tls_pem_paths": ["%s"]
+              }
+            }
+            """.formatted(certPath)
+        )
+        .header("Content-Type", "application/json")
+        .put()
+        .then()
+        .statusCode(200)
+        .extract()
+        .body()
+        .asString();
+    var responseJson = asJson(responseBody);
+
+    var expectedResponseJson = asJson(expectedResponse.formatted(certPath));
+    assertEquals(expectedResponseJson, responseJson);
+
+    // Verify that the web client trusts the provided PEM file
+    trustedCerts = getTrustedCertsOfWebClient(webClientFactory.getWebClient());
+    assertTrue(trustedCerts.contains(certPath));
+  }
+
+  @Test
+  @Order(4)
+  void updatePreferencesShouldReplaceNullValuesWithDefaults() {
+    // Make sure that we've defined preferences for `trust_all_certificates` and `tls_pem_path`
+    var expectedResponse = loadResource("preferences/update-multiple-preferences-response.json")
+        .formatted(getCertPath("certs/artificial-rootca.pem"));
+    var expectedResponseJson = asJson(expectedResponse);
+
+    var responseBody = given()
+        .when()
+        .get()
+        .then()
+        .statusCode(200)
+        .extract()
+        .body()
+        .asString();
+    var responseJson = asJson(responseBody);
+
+    assertEquals(expectedResponseJson, responseJson);
+
+    // Reset the preferences
+    var expectedResponseWithoutPreferences = asJson(
+        loadResource("preferences/get-preferences-initial-response.json")
+    );
+
+    responseBody = given()
+        .when()
+        .body(
+            """
+            {
+              "api_version": "gateway/v1",
+              "kind": "Preferences",
+              "spec": {
+                "tls_pem_paths": null,
+                "trust_all_certificates": null
+              }
+            }
+            """
+        )
+        .header("Content-Type", "application/json")
+        .put()
+        .then()
+        .statusCode(200)
+        .extract()
+        .body()
+        .asString();
+    responseJson = asJson(responseBody);
+
+    assertEquals(expectedResponseWithoutPreferences, responseJson);
+  }
+
+  @Test
+  @Order(5)
+  void updatePreferencesShouldReturnErrorIfProvidedTlsPemPathDoesNotExist() {
+    var responseBody = given()
+        .when()
+        .body(
+            """
+            {
+              "api_version": "gateway/v1",
+              "kind": "Preferences",
+              "spec": {
+                "tls_pem_paths": ["cert-does-not-exist.pem"]
+              }
+            }
+            """
+        )
+        .header("Content-Type", "application/json")
+        .put()
+        .then()
+        .statusCode(400)
+        .extract()
+        .body()
+        .asString();
+    var responseJson = asJson(responseBody);
+
+    assertNotNull(responseJson);
+    var errors = responseJson.get("errors");
+    assertNotNull(errors);
+    var error = errors.get(0);
+    assertEquals(
+        "The cert file cert-does-not-exist.pem cannot be found.",
+        error.get("detail").textValue()
+    );
+  }
+
+  /**
+   * Get the certificates trusted by the provided web client.
+   * @param webClient The web client.
+   * @return The list of certs trusted by the web client; empty list, if the web client does not
+   *         trust any custom cert.
+   */
+  List<String> getTrustedCertsOfWebClient(WebClient webClient) {
+    try {
+      var tokenField = webClient.getClass().getDeclaredField("options");
+      tokenField.setAccessible(true);
+      var options = (WebClientOptions) tokenField.get(webClient);
+      var pemTrustOptions = (PemTrustOptions) options.getTrustOptions();
+
+      if (pemTrustOptions == null) {
+        return Collections.emptyList();
+      }
+
+      return pemTrustOptions.getCertPaths();
+    } catch (NoSuchFieldException | IllegalAccessException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  /**
+   * Returns the full path of the cert with the given name in the test/resources folder.
+   * @param certName Name of the cert
+   * @return Full path to cert file.
+   */
+  String getCertPath(String certName) {
+    return Thread
+        .currentThread()
+        .getContextClassLoader()
+        .getResource(certName)
+        .getFile();
+  }
+}
