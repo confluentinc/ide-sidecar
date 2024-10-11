b239bbfc4c93132688e4deef341afae6096c72e8
Rohit Sanjay <rsanjay@confluent.io>
2024-10-11 00:27:40 -0700

    Implement Kafka REST from scratch (#74)
    * Add openapi generation and implement basic endpoint
    
    * Implement remaining endpoints
    
    * add api spec
    
    * Add cluster api and integration tests
    
    * fix null authorizedOperations
    
    * Add metadata and related urls to response
    
    * Introduce AdminClients to cache AdminClient by connection id and cluster id
    
    * refactor
    
    * broken wide
    
    * wip: needs refactor
    
    * works again
    
    * use constant var
    
    * Add tests for cluster API
    
    * Support confluent local using internal kafka rest
    
    * revert file changes
    
    * make util cass final
    
    * Add docstrings
    
    * fix
    
    * Reduce AdminClient request timeout to 10 seconds and handle exception
    
    * Use Caffeine to do time-based eviction on AdminClient instances
    
    * remove unused import
    
    * remove TODO
    
    * Put unrelated openapi spec changes back in place
    
    * Add exception mapper for UnsupportedOperationException
    
    * Set AdminClient timeout to 3 seconds
    
    * Implement Kafka REST from scratch
    
    * Add openapi generation and implement basic endpoint
    
    * Implement remaining endpoints
    
    * add api spec
    
    * Add cluster api and integration tests
    
    * fix null authorizedOperations
    
    * Add metadata and related urls to response
    
    * Introduce AdminClients to cache AdminClient by connection id and cluster id
    
    * refactor
    
    * broken wide
    
    * wip: needs refactor
    
    * works again
    
    * use constant var
    
    * Add tests for cluster API
    
    * Support confluent local using internal kafka rest
    
    * revert file changes
    
    * make util cass final
    
    * Add docstrings
    
    * fix
    
    * Reduce AdminClient request timeout to 10 seconds and handle exception
    
    * Use Caffeine to do time-based eviction on AdminClient instances
    
    * remove unused import
    
    * remove TODO
    
    * Put unrelated openapi spec changes back in place
    
    * Add exception mapper for UnsupportedOperationException
    
    * Wait until kafka-rest has started up
    
    * Hide internal kafka routes
    
    * Incorporate PR feedback
    
    * Fix graalvm/caffeine compilation
    
    * revert openapi files
    
    * remove newline
    
    * Add exception mapper for kafka ApiException
    
    * Set content-length only if transfer-encoding is not set
    
    * Support camelCase includeAuthorizedOperations query param in list topics
    
    * remove unused params
    
    * revert processProxyResponse changes
    
    * fix wording
    
    * Address reviewer feedback

---
+++ src/main/java/io/confluent/idesidecar/restapi/cache/AdminClients.java
@@ -0,0 +1,53 @@
+package io.confluent.idesidecar.restapi.cache;
+
+import com.github.benmanes.caffeine.cache.CaffeineSpec;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import java.util.Map;
+import java.util.Properties;
+import org.apache.kafka.clients.admin.AdminClient;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+/**
+ * Create an ApplicationScoped bean to cache AdminClient instances by connection ID and client ID.
+ */
+@ApplicationScoped
+public class AdminClients extends Clients<AdminClient> {
+
+  // Evict cached AdminClient instances after 5 minutes of inactivity
+  private static final String CAFFEINE_SPEC = "expireAfterAccess=5m";
+
+  @Inject
+  ClusterCache clusterCache;
+
+  @ConfigProperty(name = "ide-sidecar.admin-client-configs")
+  Map<String, String> adminClientSidecarConfigs;
+
+  public AdminClients() {
+    super(CaffeineSpec.parse(CAFFEINE_SPEC));
+  }
+
+  /**
+   * Get an AdminClient for the given connection ID and Kafka cluster ID.
+   * If the client does not already exist, it will be created.
+   * @param connectionId The connection ID
+   * @param clusterId    The cluster ID
+   * @return The AdminClient
+   */
+  public AdminClient getClient(String connectionId, String clusterId) {
+    return getClient(
+        connectionId,
+        clusterId,
+        () -> AdminClient.create(getAdminClientConfig(connectionId, clusterId))
+    );
+  }
+
+  private Properties getAdminClientConfig(String connectionId, String clusterId) {
+    var cluster = clusterCache.getKafkaCluster(connectionId, clusterId);
+    var props = new Properties();
+    // Set AdminClient configs provided by the sidecar
+    props.putAll(adminClientSidecarConfigs);
+    props.put("bootstrap.servers", cluster.bootstrapServers());
+    return props;
+  }
+}
--- src/test/java/io/confluent/idesidecar/restapi/proxy/clusters/strategy/ClusterStrategyTest.java
+++ src/test/java/io/confluent/idesidecar/restapi/proxy/clusters/strategy/ClusterStrategyTest.java
@@ -3,13 +3,19 @@ package io.confluent.idesidecar.restapi.proxy.clusters.strategy;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import io.quarkus.test.junit.QuarkusTest;
 import java.util.stream.Stream;
+import org.eclipse.microprofile.config.ConfigProvider;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+@QuarkusTest
 class ClusterStrategyTest {
+
+  static final int TEST_PORT = ConfigProvider.getConfig()
+      .getValue("quarkus.http.test-port", Integer.class);
 
   static class ClusterStrategyImpl extends ClusterStrategy {
 
@@ -27,7 +33,7 @@ class ClusterStrategyTest {
     var actualResponse = baseClusterStrategy.processProxyResponse(
         proxyResponse,
         clusterUri,
-        "http://localhost:26637"
+        "http://localhost:%s".formatted(TEST_PORT)
     );
     assertEquals(expectedResponse, actualResponse);
   }
@@ -48,10 +54,10 @@ class ClusterStrategyTest {
             """
                 {
                   "partitions": {
-                    "related": "http://localhost:26637/kafka/v3/clusters/lkc-95w6wy/topics/my_topic/partitions"
+                    "related": "http://localhost:%s/kafka/v3/clusters/lkc-95w6wy/topics/my_topic/partitions"
                   }
                 }
-                """
+                """.formatted(TEST_PORT)
         ),
         // When the returned URLs contain a port
         Arguments.of(
@@ -67,10 +73,10 @@ class ClusterStrategyTest {
             """
                 {
                   "partitions": {
-                    "related": "http://localhost:26637/kafka/v3/clusters/lkc-95w6wy/topics/my_topic/partitions"
+                    "related": "http://localhost:%s/kafka/v3/clusters/lkc-95w6wy/topics/my_topic/partitions"
                   }
                 }
-                """
+                """.formatted(TEST_PORT)
         ),
         // When the stored cluster URI has a port, but the returned URLs do not
         Arguments.of(
@@ -86,10 +92,10 @@ class ClusterStrategyTest {
             """
                 {
                   "partitions": {
-                    "related": "http://localhost:26637/kafka/v3/clusters/lkc-95w6wy/topics/my_topic/partitions"
+                    "related": "http://localhost:%s/kafka/v3/clusters/lkc-95w6wy/topics/my_topic/partitions"
                   }
                 }
-                """
+                """.formatted(TEST_PORT)
         ),
         // When both the stored cluster URI and the returned URLs have ports
         Arguments.of(
@@ -106,10 +112,10 @@ class ClusterStrategyTest {
             """
                 {
                   "partitions": {
-                    "related": "http://localhost:26637/kafka/v3/clusters/lkc-95w6wy/topics/my_topic/partitions"
+                    "related": "http://localhost:%s/kafka/v3/clusters/lkc-95w6wy/topics/my_topic/partitions"
                   }
                 }
-                """
+                """.formatted(TEST_PORT)
         ));
   }
 
