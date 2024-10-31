3489124d4cd25f66afb76b67c1926926c54b6428
Rohit Sanjay <rsanjay@confluent.io>
2024-10-31 14:31:29 -0700

    FIX: Cache networking failures when fetching schemas from SR (#132)
    * Cache network failures during schema fetch (and support retry mechanism)
    
    * temporarily increase max retry delay to 50 seconds
    
    * Ensure consume endpoint does not have possibly blocking code
    
    * set sensible defaults
    
    * narrow down to specific network related exceptions
    
    * Refactor to use Uni for retrying
    
    * weird test fix
    
    * Use response code constants
    
    * Specify schema fetch retry timeout through app config
    
    * add to app yml
    
    * Set high timeout so we don't hit it during tests
    
    * Cache wrapped exception

--- src/test/java/io/confluent/idesidecar/restapi/messageviewer/SimpleMockSchemaRegistryClient.java
+++ src/test/java/io/confluent/idesidecar/restapi/messageviewer/SimpleMockSchemaRegistryClient.java
@@ -1,16 +1,11 @@
 package io.confluent.idesidecar.restapi.messageviewer;
 
-
 import io.confluent.kafka.schemaregistry.ParsedSchema;
 import io.confluent.kafka.schemaregistry.SchemaProvider;
 import io.confluent.kafka.schemaregistry.client.MockSchemaRegistryClient;
-import io.confluent.kafka.schemaregistry.client.SchemaMetadata;
-import io.confluent.kafka.schemaregistry.client.rest.entities.Schema;
 import io.confluent.kafka.schemaregistry.client.rest.exceptions.RestClientException;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
+import java.net.ConnectException;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -18,18 +13,15 @@ import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.atomic.AtomicInteger;
 
-
 public class SimpleMockSchemaRegistryClient extends MockSchemaRegistryClient {
   private final Map<String, Map<ParsedSchema, Integer>> schemaCache;
   private final Map<Integer, ParsedSchema> idCache;
   private final AtomicInteger idCounter;
-  private Set<Integer> unauthenticated;
+  private final Set<Integer> unauthenticated;
+  private final Set<Integer> networkErrored;
 
   public SimpleMockSchemaRegistryClient() {
-    schemaCache = new HashMap<>();
-    idCache = new HashMap<>();
-    idCounter = new AtomicInteger(100001);
-    unauthenticated = new HashSet<>();
+    this(List.of());
   }
 
   public SimpleMockSchemaRegistryClient(List<SchemaProvider> list) {
@@ -38,6 +30,7 @@ public class SimpleMockSchemaRegistryClient extends MockSchemaRegistryClient {
     idCache = new HashMap<>();
     idCounter = new AtomicInteger(100001);
     unauthenticated = new HashSet<>();
+    networkErrored = new HashSet<>();
   }
 
   public int register(int requiredSchemaId, String subject, ParsedSchema schema)
@@ -49,8 +42,14 @@ public class SimpleMockSchemaRegistryClient extends MockSchemaRegistryClient {
     return requiredSchemaId;
   }
 
-  public void registerUnAuthenticated(int schemaId) {
+  public SimpleMockSchemaRegistryClient registerWithStatusCode(int schemaId, int statusCode) {
     unauthenticated.add(schemaId);
+    return this;
+  }
+
+  public SimpleMockSchemaRegistryClient registerAsNetworkErrored(int schemaId) {
+    networkErrored.add(schemaId);
+    return this;
   }
 
   @Override
@@ -66,12 +65,15 @@ public class SimpleMockSchemaRegistryClient extends MockSchemaRegistryClient {
     if (unauthenticated.contains(id)) {
       throw new RestClientException("User is denied operation on this server (mock).", 403, 40301);
     }
+    if (networkErrored.contains(id)) {
+      // Could be any network error, but throwing ConnectException since it is a common one.
+      throw new ConnectException("Network error (mock)");
+    }
     return idCache.get(id);
   }
 
   @Override
-  public ParsedSchema getSchemaBySubjectAndId(String subject, int id)
-      throws IOException, RestClientException {
+  public ParsedSchema getSchemaBySubjectAndId(String subject, int id) {
     Map<ParsedSchema, Integer> subjectSchemaCache = schemaCache.get(subject);
     if (subjectSchemaCache != null) {
       for (Map.Entry<ParsedSchema, Integer> entry : subjectSchemaCache.entrySet()) {
@@ -81,102 +83,5 @@ public class SimpleMockSchemaRegistryClient extends MockSchemaRegistryClient {
       }
     }
     return null;
-  }
-
-  @Override
-  public Collection<String> getAllSubjectsById(int id) {
-    Collection<String> subjects = new ArrayList<>();
-    for (Map.Entry<String, Map<ParsedSchema, Integer>> entry : schemaCache.entrySet()) {
-      if (entry.getValue().containsValue(id)) {
-        subjects.add(entry.getKey());
-      }
-    }
-    return subjects;
-  }
-
-  @Override
-  public SchemaMetadata getLatestSchemaMetadata(String subject) throws IOException, RestClientException {
-    Map<ParsedSchema, Integer> subjectSchemaCache = schemaCache.get(subject);
-    if (subjectSchemaCache != null && !subjectSchemaCache.isEmpty()) {
-      int latestVersion = Collections.max(subjectSchemaCache.values());
-      ParsedSchema latestSchema = getSchemaBySubjectAndId(subject, latestVersion);
-      return new SchemaMetadata((Schema) latestSchema);
-    }
-    return null;
-  }
-
-  @Override
-  public SchemaMetadata getSchemaMetadata(String subject, int version) throws IOException, RestClientException {
-    ParsedSchema schema = getSchemaBySubjectAndId(subject, version);
-    if (schema != null) {
-      return new SchemaMetadata((Schema) schema);
-    }
-    return null;
-  }
-
-  @Override
-  public int getVersion(String subject, ParsedSchema schema)
-      throws IOException, RestClientException {
-    Map<ParsedSchema, Integer> subjectSchemaCache = schemaCache.get(subject);
-    if (subjectSchemaCache != null) {
-      return subjectSchemaCache.getOrDefault(schema, -1);
-    }
-    return -1;
-  }
-
-  @Override
-  public List<Integer> getAllVersions(String subject) throws IOException, RestClientException {
-    Map<ParsedSchema, Integer> subjectSchemaCache = schemaCache.get(subject);
-    if (subjectSchemaCache != null) {
-      return new ArrayList<>(subjectSchemaCache.values());
-    }
-    return Collections.emptyList();
-  }
-
-  @Override
-  public boolean testCompatibility(String subject, ParsedSchema schema)
-      throws IOException, RestClientException {
-    return false; // Implement if needed for the test
-  }
-
-  @Override
-  public String setMode(String mode) throws IOException, RestClientException {
-    return null; // Implement if needed for the test
-  }
-
-  @Override
-  public String setMode(String mode, String subject) throws IOException, RestClientException {
-    return null; // Implement if needed for the test
-  }
-
-  @Override
-  public String getMode() throws IOException, RestClientException {
-    return null; // Implement if needed for the test
-  }
-
-  @Override
-  public String getMode(String subject) throws IOException, RestClientException {
-    return null; // Implement if needed for the test
-  }
-
-  @Override
-  public Collection<String> getAllSubjects() throws IOException, RestClientException {
-    return schemaCache.keySet();
-  }
-
-  @Override
-  public int getId(String subject, ParsedSchema schema) throws IOException, RestClientException {
-    Map<ParsedSchema, Integer> subjectSchemaCache = schemaCache.get(subject);
-    if (subjectSchemaCache != null) {
-      return subjectSchemaCache.getOrDefault(schema, -1);
-    }
-    return -1;
-  }
-
-  @Override
-  public void reset() {
-    schemaCache.clear();
-    idCache.clear();
-    idCounter.set(100001);
   }
 }
