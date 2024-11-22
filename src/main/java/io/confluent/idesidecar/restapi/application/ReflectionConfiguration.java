2f46ac6cac90f78de5808232930a611f8139ab58
Rohit Sanjay <rsanjay@confluent.io>
2024-11-22 15:09:39 -0800

    Fix deserializing records with schema references
    Adds all entity classes from the schema registry client to GraalVM native image reflection configuration.

--- src/main/java/io/confluent/idesidecar/restapi/application/ReflectionConfiguration.java
+++ src/main/java/io/confluent/idesidecar/restapi/application/ReflectionConfiguration.java
@@ -3,11 +3,31 @@ package io.confluent.idesidecar.restapi.application;
 import com.google.protobuf.Message;
 import io.confluent.idesidecar.scaffolding.models.TemplateManifest;
 import io.confluent.kafka.schemaregistry.avro.AvroSchema;
+import io.confluent.kafka.schemaregistry.client.rest.entities.Config;
 import io.confluent.kafka.schemaregistry.client.rest.entities.ErrorMessage;
 import io.confluent.kafka.schemaregistry.client.rest.entities.Schema;
 import io.confluent.kafka.schemaregistry.client.rest.entities.SchemaString;
 import io.confluent.kafka.schemaregistry.client.rest.entities.SchemaTypeConverter;
+import io.confluent.kafka.schemaregistry.client.rest.entities.SchemaEntity;
+import io.confluent.kafka.schemaregistry.client.rest.entities.SchemaEntity.EntityType;
+import io.confluent.kafka.schemaregistry.client.rest.entities.SchemaReference;
+import io.confluent.kafka.schemaregistry.client.rest.entities.SchemaRegistryServerVersion;
+import io.confluent.kafka.schemaregistry.client.rest.entities.SchemaTags;
+import io.confluent.kafka.schemaregistry.client.rest.entities.ServerClusterId;
+import io.confluent.kafka.schemaregistry.client.rest.entities.SubjectVersion;
+import io.confluent.kafka.schemaregistry.client.rest.entities.Metadata;
+import io.confluent.kafka.schemaregistry.client.rest.entities.Mode;
+import io.confluent.kafka.schemaregistry.client.rest.entities.Rule;
+import io.confluent.kafka.schemaregistry.client.rest.entities.RuleKind;
+import io.confluent.kafka.schemaregistry.client.rest.entities.RuleMode;
+import io.confluent.kafka.schemaregistry.client.rest.entities.RuleSet;
+import io.confluent.kafka.schemaregistry.client.rest.entities.ExtendedSchema;
+import io.confluent.kafka.schemaregistry.client.rest.entities.requests.ConfigUpdateRequest;
+import io.confluent.kafka.schemaregistry.client.rest.entities.requests.CompatibilityCheckResponse;
+import io.confluent.kafka.schemaregistry.client.rest.entities.requests.ModeUpdateRequest;
 import io.confluent.kafka.schemaregistry.client.rest.entities.requests.RegisterSchemaRequest;
+import io.confluent.kafka.schemaregistry.client.rest.entities.requests.RegisterSchemaResponse;
+import io.confluent.kafka.schemaregistry.client.rest.entities.requests.TagSchemaRequest;
 import io.confluent.kafka.schemaregistry.json.JsonSchema;
 import io.confluent.kafka.schemaregistry.protobuf.ProtobufSchema;
 import io.confluent.kafka.serializers.context.NullContextNameStrategy;
@@ -42,7 +62,27 @@ import org.apache.kafka.common.serialization.ByteArrayDeserializer;
         NullContextNameStrategy.class,
         TopicNameStrategy.class,
         ErrorMessage.class,
-        DefaultReferenceSubjectNameStrategy.class
+        DefaultReferenceSubjectNameStrategy.class,
+        SchemaEntity.class,
+        SchemaReference.class,
+        SchemaRegistryServerVersion.class,
+        SchemaTags.class,
+        ServerClusterId.class,
+        SubjectVersion.class,
+        Mode.class,
+        Rule.class,
+        RuleKind.class,
+        RuleMode.class,
+        Metadata.class,
+        ExtendedSchema.class,
+        TagSchemaRequest.class,
+        ConfigUpdateRequest.class,
+        CompatibilityCheckResponse.class,
+        ModeUpdateRequest.class,
+        RegisterSchemaResponse.class,
+        RuleSet.class,
+        Config.class,
+        EntityType.class
     }
 )
 public class ReflectionConfiguration {
