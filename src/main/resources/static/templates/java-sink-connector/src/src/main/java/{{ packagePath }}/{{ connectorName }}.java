862eb79cdae9cbb92f2098b09be2ed7fccbb2a3e
Stefan Sprenger <ssprenger@confluent.io>
2024-11-25 11:43:00 +0100

    chore: Update templates
    Update the templates to the latest revision of the template registry.

--- src/main/resources/static/templates/java-sink-connector/src/src/main/java/{{ packagePath }}/{{ connectorName }}.java
+++ src/main/resources/static/templates/java-sink-connector/src/src/main/java/{{ packagePath }}/{{ connectorName }}.java
@@ -5,7 +5,7 @@ import java.util.Map;
 
 import org.apache.kafka.common.config.ConfigDef;
 import org.apache.kafka.connect.connector.Task;
-import org.apache.kafka.connect.source.SinkConnector;
+import org.apache.kafka.connect.sink.SinkConnector;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
