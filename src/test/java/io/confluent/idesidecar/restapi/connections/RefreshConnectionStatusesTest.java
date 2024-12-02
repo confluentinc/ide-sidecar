ac5d800ecfb3649a13f7ccbc484fdb02cea65336
Stefan Sprenger <ssprenger@confluent.io>
2024-12-02 10:50:19 +0100

    test: Add more tests for `RefreshConnectionStatuses`

--- src/test/java/io/confluent/idesidecar/restapi/connections/RefreshConnectionStatusesTest.java
+++ src/test/java/io/confluent/idesidecar/restapi/connections/RefreshConnectionStatusesTest.java
@@ -2,25 +2,66 @@ package io.confluent.idesidecar.restapi.connections;
 
 import io.quarkus.test.junit.QuarkusTest;
 import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 @QuarkusTest
 public class RefreshConnectionStatusesTest {
 
+  CCloudConnectionState confluentCloudConnection = Mockito.mock(CCloudConnectionState.class);
+  LocalConnectionState localConnection = Mockito.mock(LocalConnectionState.class);
+  DirectConnectionState directConnection = Mockito.mock(DirectConnectionState.class);
+  List<ConnectionState> mockedConnections = List.of(
+      confluentCloudConnection,
+      localConnection,
+      directConnection
+  );
+  ConnectionStateManager connectionStateManager = Mockito.spy(ConnectionStateManager.class);
+  RefreshConnectionStatuses refreshConnectionStatuses;
+
+  @BeforeEach
+  void setup() {
+    Mockito.when(connectionStateManager.getConnectionStates()).thenReturn(mockedConnections);
+    refreshConnectionStatuses = new RefreshConnectionStatuses(connectionStateManager);
+  }
+
+  @Test
+  void refreshConfluentCloudConnectionStatusesShouldRefreshOnlyCCloudConnections() {
+    refreshConnectionStatuses.refreshConfluentCloudConnectionStatuses();
+
+    Mockito.verify(confluentCloudConnection, Mockito.times(1)).refreshStatus();
+    Mockito.verify(localConnection, Mockito.never()).refreshStatus();
+    Mockito.verify(directConnection, Mockito.never()).refreshStatus();
+  }
+
+  @Test
+  void refreshLocalConnectionStatusesShouldRefreshOnlyLocalConnections() {
+    refreshConnectionStatuses.refreshLocalConnectionStatuses();
+
+    Mockito.verify(confluentCloudConnection, Mockito.never()).refreshStatus();
+    Mockito.verify(localConnection, Mockito.times(1)).refreshStatus();
+    Mockito.verify(directConnection, Mockito.never()).refreshStatus();
+  }
+
+  @Test
+  void refreshDirectConnectionStatusesShouldRefreshOnlyDirectConnections() {
+    refreshConnectionStatuses.refreshDirectConnectionStatuses();
+
+    Mockito.verify(confluentCloudConnection, Mockito.never()).refreshStatus();
+    Mockito.verify(localConnection, Mockito.never()).refreshStatus();
+    Mockito.verify(directConnection, Mockito.times(1)).refreshStatus();
+  }
+
   @Test
   void refreshConnectionStatusesShouldRefreshOnlyConnectionsMatchingProvidedPredicate() {
-    var connectionStateManager = Mockito.spy(ConnectionStateManager.class);
-    var confluentCloudConnection = Mockito.spy(CCloudConnectionState.class);
-    var localConnection = Mockito.spy(LocalConnectionState.class);
-    Mockito.when(connectionStateManager.getConnectionStates())
-        .thenReturn(List.of(confluentCloudConnection, localConnection));
+    // Refresh the statuses of only CCloud and Local connections
+    refreshConnectionStatuses.refreshConnectionStatuses(connection ->
+        connection instanceof CCloudConnectionState || connection instanceof LocalConnectionState
+    );
 
-    var refreshConnectionStatuses = new RefreshConnectionStatuses(connectionStateManager);
-
-    refreshConnectionStatuses.refreshConnectionStatuses(CCloudConnectionState.class::isInstance);
-
-    Mockito.verify(confluentCloudConnection, Mockito.atLeastOnce()).refreshStatus();
-    Mockito.verify(localConnection, Mockito.never()).refreshStatus();
+    Mockito.verify(confluentCloudConnection, Mockito.times(1)).refreshStatus();
+    Mockito.verify(localConnection, Mockito.times(1)).refreshStatus();
+    Mockito.verify(directConnection, Mockito.never()).refreshStatus();
   }
 }
