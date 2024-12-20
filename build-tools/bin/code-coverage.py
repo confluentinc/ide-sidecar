ecbe488b60303f5031ad21c0da9db752368c23d5
Rohit Sanjay <rsanjay@confluent.io>
2024-12-20 12:23:04 -0800

    chore: Remove dead code and scripts (#250)

--- build-tools/bin/code-coverage.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-from bs4 import BeautifulSoup
-
-with open("target/site/jacoco-aggregate/index.html", mode='r') as html_file:
-    html_doc = html_file.read()
-    soup = BeautifulSoup(html_doc, 'html.parser')
-    print(soup.find(id="coveragetable").find("tfoot").find("td", class_="ctr2").text)
