b6176c12c1960dc3b0d59f37ef81cb4df8a41abb
Cerchie <lcerchie@confluent.io>
2025-03-31 12:43:33 -0700

    add new exception

--- src/main/java/io/confluent/idesidecar/restapi/proxy/CCloudApiProcessor.java
+++ src/main/java/io/confluent/idesidecar/restapi/proxy/CCloudApiProcessor.java
@@ -3,6 +3,7 @@ package io.confluent.idesidecar.restapi.proxy;
 import static io.vertx.core.http.HttpHeaders.AUTHORIZATION;
 
 import io.confluent.idesidecar.restapi.connections.CCloudConnectionState;
+import io.confluent.idesidecar.restapi.exceptions.ControlPlaneTokenNotFoundException;
 import io.confluent.idesidecar.restapi.exceptions.ProcessorFailedException;
 import io.confluent.idesidecar.restapi.processors.Processor;
 import io.confluent.idesidecar.restapi.util.UriUtil;
@@ -25,17 +26,17 @@ public class CCloudApiProcessor extends Processor<ProxyContext, Future<ProxyCont
   public Future<ProxyContext> process(ProxyContext context) {
     String clusterId = context.getRequestHeaders().get("X-Cluster-Id");
     var connectionState = context.getConnectionState();
-    if (connectionState instanceof CCloudConnectionState cCloudConnection) {
+    if (clusterId != null && !clusterId.isEmpty()) {
+      return Future.failedFuture(
+          new ProcessorFailedException(
+              context.fail(400, "This route does not support the request header 'X-Cluster-Id'."
+                  + " Please remove the header and try again.")));
+    } else if (connectionState instanceof CCloudConnectionState cCloudConnection) {
       return getControlPlaneToken(context, cCloudConnection).compose(v -> {
         context.setProxyRequestAbsoluteUrl(uriUtil.combine(ccloudApiBaseUrl,
             context.getRequestUri()));
         return next().process(context);
       });
-    } else if (clusterId != null && !clusterId.isEmpty()) {
-      return Future.failedFuture(
-          new ProcessorFailedException(
-              context.fail(400, "This route does not support the request header 'X-Cluster-Id'."
-                  + " Please remove the header and try again.")));
     } else {
       return Future.failedFuture(
           new ProcessorFailedException(
@@ -43,17 +44,30 @@ public class CCloudApiProcessor extends Processor<ProxyContext, Future<ProxyCont
     }
   }
 
-  static Future getControlPlaneToken(
+  /**
+   * Adds the control plane token to the provided context's request headers or returns a failed Future
+   * with a CCloudAuthenticationFailedException if the token is missing.
+   *
+   * @param context The proxy context to add the authorization header to
+   * @param cCloudConnection The CCloud connection state containing the OAuth context with the token
+   * @return A succeeded Future if the token was added, or a failed Future with CCloudAuthenticationFailedException
+   */
+  static Future<Void> getControlPlaneToken(
       ProxyContext context, CCloudConnectionState cCloudConnection) {
     var controlPlaneToken = cCloudConnection.getOauthContext().getControlPlaneToken();
+
+    // If token is missing, return a failed future with ControlPlaneTokenNotFoundException
     if (controlPlaneToken == null) {
       return Future.failedFuture(
-          new ProcessorFailedException(context.fail(401, "Not authenticated with Confluent Cloud")));
+          new ControlPlaneTokenNotFoundException("Control plane token not found"));
     }
+
+    // Token exists, add it to the headers
     var headers = context.getProxyRequestHeaders() != null ? context.getProxyRequestHeaders()
         : MultiMap.caseInsensitiveMultiMap();
     headers.add(AUTHORIZATION, "Bearer %s".formatted(controlPlaneToken.token()));
     context.setProxyRequestHeaders(headers);
+
     return Future.succeededFuture();
   }
 }
