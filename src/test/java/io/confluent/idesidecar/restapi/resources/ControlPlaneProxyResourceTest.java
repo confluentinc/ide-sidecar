97c7c62d82a800164c49b12e88ff2fba6459dbe1
Cerchie <lcerchie@confluent.io>
2025-03-31 08:56:30 -0700

    add test

--- src/test/java/io/confluent/idesidecar/restapi/resources/ControlPlaneProxyResourceTest.java
+++ src/test/java/io/confluent/idesidecar/restapi/resources/ControlPlaneProxyResourceTest.java
@@ -10,11 +10,20 @@ import com.github.tomakehurst.wiremock.matching.EqualToPattern;
 import io.confluent.idesidecar.restapi.connections.CCloudConnectionState;
 import io.confluent.idesidecar.restapi.connections.ConnectionStateManager;
 import io.confluent.idesidecar.restapi.models.ConnectionSpec.ConnectionType;
+import io.confluent.idesidecar.restapi.processors.Processor;
+import io.confluent.idesidecar.restapi.proxy.ControlPlaneProxyProcessor;
+import io.confluent.idesidecar.restapi.proxy.ProxyContext;
 import io.confluent.idesidecar.restapi.testutil.NoAccessFilterProfile;
 import io.confluent.idesidecar.restapi.util.CCloudTestUtil;
 import io.quarkiverse.wiremock.devservice.ConnectWireMock;
+import io.quarkus.logging.Log;
 import io.quarkus.test.junit.QuarkusTest;
 import io.quarkus.test.junit.TestProfile;
+import io.vertx.core.Future;
+import io.vertx.core.MultiMap;
+import io.vertx.core.buffer.Buffer;
+import io.vertx.core.http.HttpHeaders;
+import io.vertx.core.http.HttpMethod;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.core.MediaType;
 import java.util.Map;
@@ -89,6 +98,7 @@ class ControlPlaneProxyResourceTest {
         .contentType(MediaType.APPLICATION_JSON)
         .body("title", is("Connection id=%s not found".formatted(CONNECTION_ID)));
   }
+
   @Test
   void testControlPlaneProxyAgainstCCloud() throws Throwable {
     // Given an authenticated CCloud connection
@@ -140,5 +150,41 @@ class ControlPlaneProxyResourceTest {
 
     // Then the response body should be the same as the expected response body
     assertEquals(expectedResponseBody, actualResponseBody);
+  }
+  @Test
+  void testControlPlaneProxyProcessorNoInitialHeaders() {
+    // Given a ProxyContext with no initial headers
+    var proxyContext = new ProxyContext(
+        "http://localhost/test",
+        null,
+        HttpMethod.PUT,
+        Buffer.buffer("TestBody"),
+        Map.of(),
+        "test-connection-id"
+    );
+
+    // Create a mock processor to be the next in the chain
+    Processor<ProxyContext, Future<ProxyContext>> nextProcessor = new Processor<>() {
+      @Override
+      public Future<ProxyContext> process(ProxyContext context) {
+        return Future.succeededFuture(context);
+      }
+    };
+
+    // Create an instance of ControlPlaneProxyProcessor and set the next processor
+    ControlPlaneProxyProcessor processor = new ControlPlaneProxyProcessor();
+    processor.setNext(nextProcessor);
+
+    // When the ControlPlaneProxyProcessor processes the context
+    var result = processor.process(proxyContext).result();
+
+    // Then the headers should include Content-Type: application/json
+    assertEquals("application/json", result.getProxyRequestHeaders().get(HttpHeaders.CONTENT_TYPE));
+    // And the absolute URL should be set correctly
+    assertEquals("http://localhost/test", result.getProxyRequestAbsoluteUrl());
+    // And the request method should be PUT
+    assertEquals(HttpMethod.PUT, result.getProxyRequestMethod());
+    // And the request body should be "TestBody"
+    assertEquals("TestBody", result.getProxyRequestBody().toString());
   }
 }
