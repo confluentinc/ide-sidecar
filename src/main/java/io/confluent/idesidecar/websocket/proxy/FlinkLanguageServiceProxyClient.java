5007ff8c66fac7a97002f2c99b3bfaa56a0b7fe1
Stefan Sprenger <ssprenger@confluent.io>
2025-06-17 09:17:40 +0200

    chore: Add log statements to language service proxy
    Add more log statements to the Flink language service proxy,
    so that we can better debug the behavior of the language server
    in VS Code.

--- src/main/java/io/confluent/idesidecar/websocket/proxy/FlinkLanguageServiceProxyClient.java
+++ src/main/java/io/confluent/idesidecar/websocket/proxy/FlinkLanguageServiceProxyClient.java
@@ -42,6 +42,7 @@ public class FlinkLanguageServiceProxyClient implements AutoCloseable {
       var container = ContainerProvider.getWebSocketContainer();
       container.connectToServer(this, URI.create(context.getConnectUrl()));
     } catch (Exception e) {
+      Log.warnf("Failed to connect to CCloud Flink Language Service: %s", e.getMessage());
       throw new ProxyConnectionFailedException(e);
     }
   }
