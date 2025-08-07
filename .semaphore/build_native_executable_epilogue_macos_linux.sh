64a3df51ae74a87967a3f1b0af6f7ff722a842ae
Dave Shoup <dave.shoup@gmail.com>
2025-08-07 19:46:58 -0400

    add `--force` to overwrite existing native executable artifacts in CI

--- .semaphore/build_native_executable_epilogue_macos_linux.sh
+++ .semaphore/build_native_executable_epilogue_macos_linux.sh
@@ -2,4 +2,4 @@ make store-test-results-to-semaphore
 make ci-bin-sem-cache-store
 make cache-docker-images
 NATIVE_EXECUTABLE=$(find target -name "*-runner")
-artifact push workflow ${NATIVE_EXECUTABLE} --destination native-executables/$(basename ${NATIVE_EXECUTABLE}-${OS}-${ARCH})
+artifact push workflow ${NATIVE_EXECUTABLE} --destination native-executables/$(basename ${NATIVE_EXECUTABLE}-${OS}-${ARCH}) --force
