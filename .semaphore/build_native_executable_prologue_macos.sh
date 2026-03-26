fa0030ecbb64810e90db0ccb568ed891c9313ccf
Stefan Sprenger <ssprenger@confluent.io>
2026-03-26 09:16:31 +0100

    ci(macos): update bash

---
+++ .semaphore/build_native_executable_prologue_macos.sh
@@ -0,0 +1,15 @@
+checkout
+make ci-bin-sem-cache-restore
+make docker-login-ci
+make load-cached-docker-images
+git config remote.origin.fetch '+refs/heads/*:refs/remotes/origin/*'
+git fetch --all --tags
+git checkout $(sem-context get release_version)
+. vault-get-secret vscodeextension/release
+# Install SDKMAN! (https://sdkman.io/install)
+# SDKMAN! requires bash 4+
+brew install bash
+curl -s "https://get.sdkman.io?rcupdate=false" | bash
+source "$HOME/.sdkman/bin/sdkman-init.sh"
+# Install GraalVM as defined in .sdkmanrc
+sdk env install
