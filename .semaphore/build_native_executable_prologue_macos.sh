825204bfb2f00d21a00bc6305b48509d8c8de9dd
Stefan Sprenger <ssprenger@confluent.io>
2026-03-26 14:05:50 +0100

    chore: move brew install

--- .semaphore/build_native_executable_prologue_macos.sh
+++ .semaphore/build_native_executable_prologue_macos.sh
@@ -8,8 +8,8 @@ git checkout $(sem-context get release_version)
 export VAULT_ADDR="https://vault.cireops.gcp.internal.confluent.cloud"
 vault login -no-print token=$(vault write -field=token "auth/semaphore_self_hosted/login" role="default" jwt="$SEMAPHORE_OIDC_TOKEN")
 # Install SDKMAN! (https://sdkman.io/install)
+brew install bash
 curl -s "https://get.sdkman.io?rcupdate=false" | bash
 source "$HOME/.sdkman/bin/sdkman-init.sh"
 # Install GraalVM as defined in .sdkmanrc
-brew install bash
 sdk env install
